cb318b08d689ad40d8e8e52bf664ab7b11fd1131
Thorsten <t.fischer@gyko.wwschool.de>
2021-02-27 21:55:16 +0100

    Neues Paket de.gyko.gameofcolours.app und Klasse GameOfColours

--- Game of Colours/src/de/gyko/gameofcolours/Starter.java
+++ Game of Colours/src/de/gyko/gameofcolours/Starter.java
@@ -1,5 +1,10 @@
 package de.gyko.gameofcolours;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import de.gyko.gameofcolours.app.GameOfColours;
+
 /**
  * Diese Klasse startet das Spiel.
  * 
@@ -7,5 +12,24 @@ package de.gyko.gameofcolours;
  *
  */
 public class Starter {
+	
+	/**
+	 * Diese Methode erzeugt den Starter des Spiels und ruft seine run-Methode auf.
+	 *   
+	 * @param args Es werden noch keine Parameter benötigt.
+	 */
+	public static void main(String[] args) {
+		
+		(new Starter()).run();
+		
+	}
+
+	/**
+	 * Startet den Starter.
+	 */
+	private void run() {
+		GameOfColours gameOfColours = new GameOfColours();
+		Logger.getLogger("de.gyko.gameofcolours").log(Level.INFO, "Hallo Welt");
+	}
 
 }
---
+++ Game of Colours/src/de/gyko/gameofcolours/app/GameOfColours.java
@@ -0,0 +1,21 @@
+package de.gyko.gameofcolours.app;
+
+import java.util.logging.Logger;
+
+/**
+ * Das Spiel GameOfColours
+ * 
+ * 
+ * @author Thorsten
+ *
+ */
+public class GameOfColours {
+	
+	/**
+	 * Diese Variable hält den Logger für das gesamte Spiel fest.
+	 */
+	@SuppressWarnings("unused")
+	private Logger logger = Logger.getLogger("de.gyko.gameofcolours");
+	
+
+}
