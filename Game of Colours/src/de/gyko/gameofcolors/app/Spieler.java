c7c6ee2b2597d836c02903859e43f675f08d680f
Thorsten <mail@thorstenfischer.com>
2021-04-17 10:46:21 +0200

    Weiterentwicklung App

---
+++ Game of Colours/src/de/gyko/gameofcolors/app/Spieler.java
@@ -0,0 +1,12 @@
+package de.gyko.gameofcolors.app;
+
+/**
+ * Von der Klasse Spieler werden Objekte des Typs Spieler instanziiert.
+ * 
+ * Ein Spieler ist derjenige, der an einem Spiel teilnehmen kann.
+ * Ein Spieler hat eine Farbe.  
+ *
+ */
+public class Spieler {
+
+}
---
+++ Game of Colours/src/de/gyko/gameofcolors/app/test/SpielerTest.java
@@ -0,0 +1,22 @@
+package de.gyko.gameofcolors.app.test;
+
+import de.gyko.gameofcolors.app.Spieler;
+
+/**
+ * 
+ * Die Klasse SpielerTest ist nur zum Testen der Klasse Spieler gedacht.
+ * Sie enthält keinen Code, der für "Game Of Color" erforderlich ist.
+ * 
+ * @author Thorsten
+ *
+ */
+public class SpielerTest {
+	
+	public static void main(String[] args) {
+		
+		new Spieler();
+		
+	}
+	
+
+}
