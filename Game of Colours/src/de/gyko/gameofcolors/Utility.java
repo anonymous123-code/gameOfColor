05a6cb2e9e726b609a8e9141f9f15bc9d754de97
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2021-04-23 20:13:22 +0200

    Ein paar nützliche Funktionen in einer Klasse

---
+++ Game of Colours/src/de/gyko/gameofcolors/Utility.java
@@ -0,0 +1,28 @@
+package de.gyko.gameofcolors;
+
+/*
+ * Some Helper Methods
+ *
+ * @author Jano Andretzky
+ */
+public class Utility {
+    public static byte[] toBytes(short... shorts){
+        byte[] result = new byte[shorts.length];
+        for (int i = 0; i < shorts.length; i++) {
+            result[i] = (byte) shorts[i];
+        }
+        return result;
+    }
+
+    public static byte[] toBytes(int... ints){
+        byte[] result = new byte[ints.length];
+        for (int i = 0; i < ints.length; i++) {
+            result[i] = (byte) ints[i];
+        }
+        return result;
+    }
+
+    public static int uint(byte b){
+        return Byte.toUnsignedInt(b);
+    }
+}
