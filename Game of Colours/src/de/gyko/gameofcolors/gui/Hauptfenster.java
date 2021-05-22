751e49844ec08b08f0e09774f2a0ba6815b77ab5
ifTiredReturnNULL <barnabas.balz@gmx.de>
2021-05-22 10:32:29 +0200

    Am GUI weitergearbeitet

--- Game of Colours/src/de/gyko/gameofcolors/gui/Hauptfenster.java
+++ Game of Colours/src/de/gyko/gameofcolors/gui/Hauptfenster.java
@@ -1,5 +1,21 @@
 package de.gyko.gameofcolors.gui;
 
-public class Hauptfenster {
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * 
+ * @author barna
+ *
+ */
+@SuppressWarnings("serial")
+public class Hauptfenster extends JFrame {
+	public Hauptfenster() {
+		
+		JPanel panel = new PlayingSurface();
+		getContentPane().add(panel, BorderLayout.CENTER);
+	}
 
 }
--- Game of Colours/src/de/gyko/gameofcolors/gui/PlayingSurface.java
+++ Game of Colours/src/de/gyko/gameofcolors/gui/PlayingSurface.java
@@ -1,42 +1,23 @@
 package de.gyko.gameofcolors.gui;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import java.awt.Color;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
-public class PlayingSurface extends JFrame {
+/**
+ * 
+ * @author barna
+ *
+ */
 
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					PlayingSurface frame = new PlayingSurface();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+@SuppressWarnings("serial")
+public class PlayingSurface extends JPanel {
 
 	/**
 	 * Create the frame.
 	 */
 	public PlayingSurface() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+	setBackground(Color.BLUE);
 	}
 
 }
