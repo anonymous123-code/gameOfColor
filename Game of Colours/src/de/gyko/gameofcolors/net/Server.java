7e609587c764ec5951e47375d78b96032bdba497
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2021-05-29 09:26:48 +0200

    (Vorerst) finale Version des Servers

--- Game of Colours/src/de/gyko/gameofcolors/net/Server.java
+++ Game of Colours/src/de/gyko/gameofcolors/net/Server.java
@@ -14,30 +14,15 @@ import java.util.concurrent.Executors;
  */
 public class Server implements Runnable{
 
-    private PacketReceiveListener packetReceiveListener;
-    Map<String, String> map = Collections.synchronizedMap(new HashMap<>());
+    private final PacketReceiveListener packetReceiveListener;
     private boolean closed = false;
     private final ArrayList<OutputStream> channels = new ArrayList<>();
     int port;
 
-    /**
-     *
-     *
-     * @param port Port für den Server
-     */
     public Server (PacketReceiveListener packetReceiveListener,int port){
         this.port = port;
         this.packetReceiveListener = packetReceiveListener;
         new Thread(this).start();
-    }
-
-    public static void main(String... args){
-        new Server(new PacketReceiveListener() {
-            @Override
-            public PacketSendRequest[] onPacketReceived(PacketReceiveEvent p) {
-                return new PacketSendRequest[0];
-            }
-        },6000);
     }
 
 
@@ -47,7 +32,7 @@ public class Server implements Runnable{
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             while (!closed) {
                 Socket socket = serverSocket.accept();
-                ServerThread serverThread = new ServerThread(socket, packetReceiveListener, channels, map);
+                ServerThread serverThread = new ServerThread(socket, packetReceiveListener, channels);
                 threadPool.submit(serverThread);
             }
         } catch (IOException e) {
