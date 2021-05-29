05ef471107c9d06f15e07174f6fff689a9a47aac
Thorsten <mail@thorstenfischer.com>
2021-05-29 11:24:59 +0200

    Mehr Funktionalität im Starter, neue Klasse "BattlefieldTest"

--- Game of Colours/src/de/gyko/gameofcolors/Starter.java
+++ Game of Colours/src/de/gyko/gameofcolors/Starter.java
@@ -1,9 +1,11 @@
 package de.gyko.gameofcolors;
 
+import java.awt.EventQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import de.gyko.gameofcolors.app.GameOfColors;
+import de.gyko.gameofcolors.gui.MainWindow;
 
 /**
  * Diese Klasse startet das Spiel.
@@ -16,7 +18,7 @@ public class Starter {
 	/**
 	 * Diese Methode erzeugt den Starter des Spiels und ruft seine run-Methode auf.
 	 *   
-	 * @param args Es werden noch keine Parameter benÃ¶tigt.
+	 * @param args Es werden noch keine Parameter benötigt.
 	 */
 	public static void main(String[] args) {
 		
@@ -28,9 +30,50 @@ public class Starter {
 	 * Startet den Starter.
 	 */
 	private void run() {
-		@SuppressWarnings("unused")
-		GameOfColors gameOfColors = new GameOfColors();
+		initializeApplication();
+		initializeNet();
+//		createAndShowConnectingDialog();
+		GameOfColors newGame = createGame();
+		createAndShowMainWindow(newGame);
+	}
+
+	/**
+	 * Initialisiert die Anwendung
+	 */
+	private void initializeApplication() {
 		Logger.getLogger("de.gyko.gameofcolours").log(Level.INFO, "Hallo Welt");
 	}
+
+	/**
+	 * Initialisiert die Netzwerkschicht
+	 */
+	private void initializeNet() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Erzeugt ein neues Spiel mit den Parametern, die im ConnectionDialog dafür angegeben waren.
+	 */
+	private GameOfColors createGame(/* Spielparameter */) {
+		GameOfColors gameOfColors = new GameOfColors();
+		return gameOfColors;
+	}
+	
+	/**
+	 * Erzeugt das GUI für das Spiel und zeigt es.
+	 * @param newGame 
+	 */
+	private void createAndShowMainWindow(GameOfColors newGame) {
+		MainWindow mainWindow = new MainWindow();
+		// TODO: Dem mainWindow das Spiel übergeben.
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				mainWindow.setVisible(true);
+			}
+		});		
+	}
+
 
 }
---
+++ Game of Colours/test/de/gyko/gameofcolors/app/BattlefieldTest.java
@@ -0,0 +1,23 @@
+package de.gyko.gameofcolors.app;
+
+import java.awt.Color;
+
+public class BattlefieldTest {
+	
+	
+
+	public static void main(String[] args) {
+		Battlefield einSchlachtfeld = new Battlefield();
+		
+		einSchlachtfeld.setHeight(5);
+		einSchlachtfeld.setWidth(10);
+		
+		einSchlachtfeld.setColorAt(1, 2, Color.BLUE);
+		
+		
+		
+		
+		
+	}
+	
+}
