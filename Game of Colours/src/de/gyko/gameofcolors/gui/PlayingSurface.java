ce49c3bdc69ae457e4316e1a3f78985acd7edba8
ifTiredReturnNULL <barnabas.balz@gmx.de>
2021-05-29 09:58:39 +0200

    Weiter an der PlayingSurface gearbeitet.

--- Game of Colours/src/de/gyko/gameofcolors/gui/PlayingSurface.java
+++ Game of Colours/src/de/gyko/gameofcolors/gui/PlayingSurface.java
@@ -1,6 +1,8 @@
 package de.gyko.gameofcolors.gui;
 
 import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
@@ -17,6 +19,14 @@ public class PlayingSurface extends JPanel {
 	 * Create the frame.
 	 */
 	public PlayingSurface() {
+		addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("Hallo");
+			}
+		});
+		//TODO
+		
 	setBackground(Color.BLUE);
 	}
 
