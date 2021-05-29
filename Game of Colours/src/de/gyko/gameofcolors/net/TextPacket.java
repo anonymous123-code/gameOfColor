21be7ebd1bfe34724f37a146d7f8e08dbc9b8215
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2021-05-29 10:33:09 +0200

    Text Feld hinzugefügt

--- Game of Colours/src/de/gyko/gameofcolors/net/TextPacket.java
+++ Game of Colours/src/de/gyko/gameofcolors/net/TextPacket.java
@@ -2,30 +2,50 @@ package de.gyko.gameofcolors.net;
 
 import java.nio.charset.StandardCharsets;
 
-/*
- * A simple Text packet
+/**
+ * Ein einfaches Text Packet
  *
  * @author Jano Andretzky
  */
 public class TextPacket extends Packet{
-    public TextPacket(String s) {
+    String text;
+
+    /**
+     * Erstellt ein neues TextPacket auf Basis eines Textes.
+     *
+     * @param text der Text des Packets
+     */
+    public TextPacket(String text) {
         super();
         // TODO: aktuell nur fuer Testzwecke, richtig machen
-        byte[] rawContent = new byte[3+s.getBytes(StandardCharsets.UTF_8).length];
+        this.text = text;
+        byte[] rawContent = new byte[3+text.getBytes(StandardCharsets.UTF_8).length];
         rawContent[0] = "txt".getBytes(StandardCharsets.UTF_8)[0];
         rawContent[1] = "txt".getBytes(StandardCharsets.UTF_8)[1];
         rawContent[2] = "txt".getBytes(StandardCharsets.UTF_8)[2];
-        byte[] sBytes = s.getBytes(StandardCharsets.UTF_8);
+        byte[] sBytes = text.getBytes(StandardCharsets.UTF_8);
         System.arraycopy(sBytes, 0, rawContent, 3, sBytes.length + 3 - 3);
         this.rawContent = rawContent;
         id = new String(new byte[]{rawContent[0], rawContent[1], rawContent[2]});
     }
 
+    /**
+     * Gibt den rohen Inhalt des Packets zurueck.
+     * @return den rohe Inhalt
+     */
     @Override
     public byte[] getRawContent() {
-        // TODO: Workaround für Testzwecke, Später entfernen
+        // TODO: Workaround fuer Testzwecke, Spaeter entfernen
         byte[] out = new byte[this.rawContent.length-3];
         System.arraycopy(this.rawContent, 3, out, 0, this.rawContent.length-3);
         return out;
+    }
+
+    /**
+     * Gibt den Text des Packets zurueck.
+     * @return den Text
+     */
+    public String getText() {
+        return text;
     }
 }
